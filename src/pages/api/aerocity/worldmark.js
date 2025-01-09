9dd3411e287f2983867315ff057a12c3592dfe85
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-01-09 21:48:25 +0530

    upsates

--- src/pages/api/aerocity/worldmark.js
+++
@@ -1,8 +0,0 @@
-
-export default function POST(req, res) {
-    var data = req.body;
-    
-    
-
-    res.status(200).json({ shopName: "Pizza Hurt" , billingName: "Shweta Nigam", amount: "1200₹" });
-  }
